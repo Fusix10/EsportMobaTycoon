6d046bc84b1757214073f3341c4406c527786b54
Kindhee <117281537+Kindhee@users.noreply.github.com>
2025-04-11 11:19:00 +0200

    RomainFinalisedCircuitClassReworwk

--- EsportMobaTycoon/Assets/Script/CircuitChoice/Circuit.cs
+++ EsportMobaTycoon/Assets/Script/CircuitChoice/Circuit.cs
@@ -1,45 +1,54 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circuit
+public abstract class StatusManager
 {
-    public class Tournament
+    public enum Status
     {
-        private enum TournamentStatus
-        {
-            NotPlayed,
-            Won,
-            Lost
-        }
+        NotPlayed,
+        Won,
+        Lost
+    }
 
+    protected Status m_status;
+
+    protected StatusManager()
+    {
+        m_status = Status.NotPlayed;
+    }
+
+    public void SetHasWon(bool win)
+    {
+        m_status = win ? Status.Won : Status.Lost;
+    }
+
+    public Status GetStatus()
+    {
+        return m_status;
+    }
+}
+
+public class Circuit : StatusManager
+{
+    public class Tournament : StatusManager
+    {
         private DateTime m_date;
-
         private bool m_is_major;
-
         private List<Match> m_matches;
-
-        private TournamentStatus m_status;
 
         public Tournament(DateTime date, bool is_major)
         {
             m_date = date;
             m_is_major = is_major;
-            m_status = TournamentStatus.NotPlayed;
-
             m_matches = new List<Match>();
-        }
-
-        public void HasWon(bool win)
-        {
-            m_status = win ? TournamentStatus.Won : TournamentStatus.Lost;
         }
 
         public void AddMatch(Match match)
         {
-            m_matches.Add(match);   
+            m_matches.Add(match);
         }
+
         public List<Match> GetMatches()
         {
             return m_matches;
@@ -56,48 +65,20 @@ public class Circuit
         }
     }
 
-    public class Match
+    public class Match : StatusManager
     {
-        private enum MatchStatus
-        {
-            NotPlayed,
-            Won,
-            Lost
-        }
-
-        private MatchStatus m_status;
-
-        public Match()
-        {
-            m_status = MatchStatus.NotPlayed;
-        }
-
-        public void HasWon(bool win)
-        {
-            m_status = win ? MatchStatus.Won : MatchStatus.Lost;
-        }
+        public Match() { }
     }
 
-    private int id;
+    private int m_id;
     private List<Tournament> m_tournaments;
 
-
-    public Circuit(int circuitId)
+    public Circuit(int circuit_id)
     {
-        id = circuitId;
-
+        m_id = circuit_id;
         m_tournaments = new List<Tournament>();
     }
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
     public void AddTournament(Tournament tournament)
     {
         m_tournaments.Add(tournament);
@@ -108,4 +89,8 @@ public class Circuit
         return m_tournaments;
     }
 
+    public int GetCircuitId()
+    {
+        return m_id;
+    }
 }
