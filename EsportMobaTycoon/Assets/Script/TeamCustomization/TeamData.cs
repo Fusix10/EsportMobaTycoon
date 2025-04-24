90e2c6a8655adf694b50fdf2fe0b47843f620920
ManaIron <117281679+ManaIron@users.noreply.github.com>
2025-04-24 21:47:06 +0200

    ManaariiCodeIncomplet

--- EsportMobaTycoon/Assets/Script/TeamCustomization/TeamData.cs
+++ EsportMobaTycoon/Assets/Script/TeamCustomization/TeamData.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TeamData", menuName = "TeamData")]
 public class TeamData : ScriptableObject
 {
+    [Header("Infos générales")]
     public string i_name;
     public string i_nickName;
+
+    [Header("Logos (Sprites)")]
     public Sprite i_LogoBack;
     public Sprite i_LogoCrown;
     public Sprite i_Logo;
+
+    [Header("Couleurs des logos")]
+    public Color i_LogoBackColor = Color.white;
+    public Color i_LogoCrownColor = Color.white;
+    public Color i_LogoMainColor = Color.white;
 }
