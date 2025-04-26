459f6b02d4f367ff622bd385b69de9b6723d85c5
gargoire5 <gportevin@gaming.tech>
2025-04-26 21:42:04 +0200

    GrégoireIncompletCutScene

---
+++ EsportMobaTycoon/Assets/Script/CutScene/TypewriterEffect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using TMPro; // important si tu utilises TextMeshPro
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public TMP_Text i_textComponent; 
+    public float i_delay = 0.1f;     
+
+    private string i_fullText;        
+    private string i_currentText = ""; 
+
+    public void StartTypewriter(string textToDisplay)
+    {
+        i_fullText = textToDisplay;
+        i_currentText = "";
+        StopAllCoroutines();
+        StartCoroutine(TypeText());
+    }
+
+    private IEnumerator TypeText()
+    {
+        foreach (char c in i_fullText)
+        {
+            i_currentText += c;
+            i_textComponent.text = i_currentText;
+            yield return new WaitForSeconds(i_delay);
+        }
+    }
+}
